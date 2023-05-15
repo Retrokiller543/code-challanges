b6fdd05cdfd663e78bf2c7ab80bd73f90026e3bf
Retrokiller543 <56911974+Retrokiller543@users.noreply.github.com>
2023-05-15 16:33:16 +0200

    Add project files.

---
+++ Code-Challanges/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Code_Challanges
+{
+    internal class Program
+    {
+        public static void FizzBuzz()
+        {
+            // Write a program that prints the numbers from 1 to 100.
+            // But for multiples of three, print "Fizz" instead of the number
+            // and for the multiples of five, print "Buzz."
+            // For numbers that are multiples of both three and five, print "FizzBuzz."
+        }
+
+        public static bool IsPalindrome(string input)
+        {
+            // Write a program that checks whether a given string is a palindrome (reads the same forwards and backwards).
+            // Ignore spaces and punctuation.
+            return false;
+        }
+
+        public static int Factorial(int number)
+        {
+            // Write a program that calculates the factorial of a given number using recursion.'
+            return number;
+        }
+
+        public static void PrintFibonacciSeries(int terms)
+        {
+            // Write a program that prints the Fibonacci series up to a given number of terms.
+        }
+
+        public static bool IsPrime(int number)
+        {
+            // Write a program that checks whether a given number is prime.
+            return false;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
